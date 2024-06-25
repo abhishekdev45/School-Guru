092604556b0207c98792782d88e1c3e3abc29249
abhishekdev45 <95223863+abhishekdev45@users.noreply.github.com>
2024-06-25 16:46:07 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -25,7 +25,7 @@ const connect = async () => {
     }
 };
 
-app.use(cors({origin:"http://localhost:5173", credentials:true}));
+app.use(cors({origin:"https://school-gurufrontend-c7vefl6s8-abhishekdev45s-projects.vercel.app", credentials:true}));
 app.use(express.json());
 app.use(cookieParser())
 
